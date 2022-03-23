45f1b855276976ad6603b6fbe6c1f47e2a7fac16
Mili Sallo <MilagrosSallo@gmail.com>
2022-03-22 21:44:52 -0300

    Cree un algoritmo que solicita nombre de usuario

---
+++ index.js
@@ -0,0 +1,7 @@
+let usuarioIngresado = prompt ("Ingresá tu nombre de usuario");
+while (usuarioIngresado != "exb788"){
+    alert ("Usuario incorrecto, vuelva a intentar");
+    usuarioIngresado = prompt ("Ingresá tu nombre de usuario");
+}
+alert ("Bienvenido al sistema");
+
